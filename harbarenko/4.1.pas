0ed40858aa76e46a2baf742544dfcd6ac9e86ca3
aleksey harbarenko <alexgar@ukr.net>
2016-07-25 18:26:28 +0300

    best wishes

---
+++ harbarenko/4.1.pas
@@ -0,0 +1,24 @@
+program best_wishes;
+uses crt;
+var 
+	i : Integer;
+	indent : String;
+		begin
+			indent := '';
+			for i:=1 to 20 do
+			begin
+			textcolor(i);
+				if i = 16 then
+					begin 
+						textcolor(i-14);
+						writeln(indent,i,' Alex, you the kindest man in the world. Your kindness warms even in winter');
+					end
+				else
+					begin
+						writeln(indent,i,' Alex, you the kindest man in the world. Your kindness warms even in winter');
+					end;
+			indent := indent + ' ';
+			delay(1000);
+			end;
+			readln();
+		end.
