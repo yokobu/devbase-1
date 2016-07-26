5fe30ea4c9a907a7b054405a6bf7bc4ddd4ab96c
aleksey harbarenko <alexgar@ukr.net>
2016-07-26 13:45:15 +0300

    new file

--- harbarenko/4.5.pas
+++ harbarenko/4.5.pas
@@ -3,24 +3,18 @@ uses crt;
 var 
 	i : Integer;
 			begin
-				for i:=1 to 99 do
+				for i:=1 to 98 do
 					begin
-						if i<>99 then
-							begin
-								textcolor(8);
-								writeln(100-i, ' bottles of beer on the wall, ', 100-i, ' bottles of beer.');
-								writeln('Take one down and pass it around, ', 100-i-1, ' bottles of beer on the wall.');
-								writeln();
-								delay(1000);
-							end
-						else
-							begin
-								writeln(100-i, ' bottle of beer on the wall, ', 100- i, ' bottle of beer.');
-								writeln('Take one down and pass it around, no more bottles of beer on the wall.');
-								writeln();
-						end;
+						textcolor(8);
+						writeln(100-i, ' bottles of beer on the wall, ', 100-i, ' bottles of beer.');
+						writeln('Take one down and pass it around, ', 100-i-1, ' bottles of beer on the wall.');
+						writeln();
+						delay(10);
 					end;
+				writeln(100-i-1, ' bottle of beer on the wall, ', 100-i-1, ' bottle of beer.');
+				writeln('Take one down and pass it around, no more bottles of beer on the wall.');
+				writeln();
 				writeln('No more bottles of beer on the wall, no more bottles of beer.');
-				writeln('Go to the store and buy some more, ', i, ' bottles of beer on the wall.');
+				writeln('Go to the store and buy some more, ', i + 1, ' bottles of beer on the wall.');
 		readln();
 		end.
