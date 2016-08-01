e1556c02876a813f43acd0e6d8dbfef5388943e6
aleksey harbarenko <alexgar@ukr.net>
2016-08-02 00:22:18 +0300

    fixed maxnumber without IF

--- harbarenko/maxnumber.pas
+++ harbarenko/maxnumber.pas
@@ -2,54 +2,20 @@ program maxnumber;
 var
 	A : Integer;
 	B : Integer;
+	i : Integer;
 		begin
 		writeln('Enter two numbers');
 		readln(A);
 		readln(B);
-			while A = B do
-				begin
-				writeln('Numbers are even');
-				break;
-				end;
-			while (A <= 0) and (B < 0) and (A div B < 1) and (B <> 0) do
-				begin
-				writeln(A, ' is bigger');
-				break;
-				end;
-			while (A <= 0) and (B < 0) and (A div B >= 1) and (B <> 0) do
-				begin
-				writeln(B, ' is bigger');
-				break;
-				end;
-			while (A >= 0) and (B > 0) and (A div B < 1) and (B <> 0) do
-				begin
-				writeln(B, ' is bigger');
-				break;
-				end;
-			while (A >= 0) and (B > 0) and (A div B >= 1) and (B <> 0) do
-				begin
-				writeln(A, ' is bigger');
-				break;
-				end;
-			while (A < 0) and (B > 0) do
-				begin
-				writeln(B, ' is bigger');
-				break;
-				end;
-			while (A > 0) and (B < 0) do
-				begin
-				writeln(A, ' is bigger');
-				break;
-				end;
-			while (A < 0) and (B = 0) do
-				begin
-				writeln(B, ' is bigger');
-				break;
-				end;
-			while (A > 0) and (B = 0) do
-				begin
-				writeln(A, ' is bigger');
-				break;
-				end;
-			readln();
+		for i := A to B do
+		begin
+		writeln(B, ' is the biggest');
+		break;
+		end;
+		for i := B to A do
+		begin
+		writeln(A, ' is the biggest');
+		break;
+		end;
+		readln();
 	end.
